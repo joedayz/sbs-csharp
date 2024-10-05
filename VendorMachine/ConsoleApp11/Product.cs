eca1a47de3881c60c744df0312838f6033ce6736
Jose Diaz <jose@exoreaction.com>
2024-10-04 20:27:19 -0500

    reto 1 de herencia

---
+++ VendorMachine/ConsoleApp11/Product.cs
@@ -0,0 +1,61 @@
+namespace ConsoleApp11;
+
+class Product {
+
+    // Private Data Members
+    private string name;
+    private double price;
+
+    // Getter Function
+    public string GetName(int liters) {
+        if (liters == 1) {
+            this.name = "Cola";
+            return this.name;
+        }
+        else if (liters == 2) {
+            this.name = "Fanta";
+            return this.name;
+        }
+        else if (liters == 3) {
+            this.name = "Dew";
+            return this.name;
+        }
+        else return "";
+    }
+
+    // Getter Function
+    public double GetPrice(int liters)
+    {
+        if (liters == 1) {
+            this.price = 2;
+            return this.price;
+        }
+        
+        else if (liters == 2) {
+            this.price = 3.5;
+            return this.price;
+        }
+    
+        else if (liters == 3) {
+            this.price = 4;
+            return this.price;
+        }
+        else return 0;
+    }
+}
+
+// Derived Class
+class Beverage { 
+
+    public int Liters { get; set; } //  Liters of a Beverage
+
+    // This function calls the Base class getters and prepends the values to the Litres
+    public String GetDetails() { 
+        string details = "";
+
+        // write your code here
+        // Return format should be "name, price, liters"
+        return details;
+    }
+
+}
