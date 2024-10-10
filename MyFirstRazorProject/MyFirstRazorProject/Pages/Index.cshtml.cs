de71dcc47254e9ba8c8e4f02657dcdc93e303cda
Jose Diaz <jose@exoreaction.com>
2024-10-09 20:28:57 -0500

    revisando los net templates

---
+++ MyFirstRazorProject/MyFirstRazorProject/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstRazorProject.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public string Message { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+        Message = "Hello, World!";
+    }
+
+    public void OnGet()
+    {
+
+        
+    }
+}
