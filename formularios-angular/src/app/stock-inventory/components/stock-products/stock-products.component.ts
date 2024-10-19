fc26e2d1596d9d76893887d6f5f9f9a0b8561749
Jose Diaz <jose@exoreaction.com>
2024-10-18 20:05:41 -0500

    eliminando elementos del formarray

--- formularios-angular/src/app/stock-inventory/components/stock-products/stock-products.component.ts
+++ formularios-angular/src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,42 +1,49 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormArray, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
-          standalone: true,
-          imports: [CommonModule, ReactiveFormsModule],
-          selector: 'stock-products',
-          template: `
-                    <div class="stock-product" [formGroup]="parent">
-                              <div formArrayName="stock">
-                                        <div *ngFor="let item of stocks; let i = index;">
-                                                  <div class="stock-product__content" [formGroupName]="i">
-                                                            <div class="stock-product__name">
-                                                                      {{ item.value.product_id }}
-                                                            </div>
-                                                            <input type="number"
-                                                            step="10"
-                                                            min="10"
-                                                            max="1000"
-                                                            formControlName="quantity"
-                                                            placeholder="Quantity" />
-                                                            <button type="button">
-                                                                      Remove
-                                                            </button>
-                                                  </div>
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  selector: 'stock-products',
+  template: `
+    <div class="stock-product" [formGroup]="parent">
+      <div formArrayName="stock">
+        <div *ngFor="let item of stocks; let i = index;">
+          <div class="stock-product__content" [formGroupName]="i">
+            <div class="stock-product__name">
+              {{ item.value.product_id }}
+            </div>
+            <input type="number"
+                   step="10"
+                   min="10"
+                   max="1000"
+                   formControlName="quantity"
+                   placeholder="Quantity"/>
+            <button type="button" (click)="onRemove(item, i)">
+              Remove
+            </button>
+          </div>
 
-                                        </div>
-                              </div>
-                    </div>
-          `,
-          styleUrl: './stock-products.component.scss'
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrl: './stock-products.component.scss'
 })
 
-export class StockProductsComponent  {
-          @Input() 
-          parent: FormGroup = new FormGroup({});
+export class StockProductsComponent {
+  @Input()
+  parent: FormGroup = new FormGroup({});
 
-          get stocks(){
-                    return (this.parent.get('stock') as FormArray).controls;
-          }
+  @Output()
+  removed = new EventEmitter<any>();
+
+  get stocks() {
+    return (this.parent.get('stock') as FormArray).controls;
+  }
+
+  onRemove(group: any, index: any) {
+    this.removed.emit({group, index});
+  }
 }
--- formularios-angular/src/app/stock-inventory/containers/stock-inventory.component.ts
+++ formularios-angular/src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,83 +1,89 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { StockBranchComponent } from '../components/stock-branch/stock-branch.component';
-import { StockProductsComponent } from '../components/stock-products/stock-products.component';
-import { StockSelectorComponent } from '../components/stock-selector/stock-selector.component';
-import { Product } from '../models/product.interface';
+import {CommonModule} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {StockBranchComponent} from '../components/stock-branch/stock-branch.component';
+import {StockProductsComponent} from '../components/stock-products/stock-products.component';
+import {StockSelectorComponent} from '../components/stock-selector/stock-selector.component';
+import {Product} from '../models/product.interface';
 
 @Component({
-          standalone: true,
-          imports: [CommonModule, ReactiveFormsModule, StockBranchComponent, StockProductsComponent, StockSelectorComponent],
-          selector: 'stock-inventory',
-          template: `
-                    <div class="stock-inventory">
-                              <form [formGroup]="form" (ngSubmit)="onSubmit()">
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, StockBranchComponent, StockProductsComponent, StockSelectorComponent],
+  selector: 'stock-inventory',
+  template: `
+    <div class="stock-inventory">
+      <form [formGroup]="form" (ngSubmit)="onSubmit()">
 
-                                        <stock-branch [parent]="form">
-                                        </stock-branch>
+        <stock-branch [parent]="form">
+        </stock-branch>
 
-                                        <stock-selector
-                                                  [parent]="form"
-                                                  [products]="products"
-                                                    (added)="addStock($event)">
-                                        </stock-selector>
+        <stock-selector
+          [parent]="form"
+          [products]="products"
+          (added)="addStock($event)">
+        </stock-selector>
 
-                                        <stock-products [parent]="form">
-                                        </stock-products>
+        <stock-products [parent]="form"
+                        (removed)="removeStock($event)">
+        </stock-products>
 
 
-                                        <div class = "stock-inventory__buttons">
-                                                  <button type="submit" [disabled]="form.invalid">
-                                                            Order Stock
-                                                  </button>
-                                        </div>
+        <div class="stock-inventory__buttons">
+          <button type="submit" [disabled]="form.invalid">
+            Order Stock
+          </button>
+        </div>
 
-                                        <pre>{{  form.value | json }}</pre>
-                              </form>
-                    </div>
-          `
+        <pre>{{ form.value | json }}</pre>
+      </form>
+    </div>
+  `
 })
 
 export class StockInventoryComponent {
 
-          products: Product[] = [
-                    { "id": 1, "price": 2800, "name": "MacBook Pro" },
-                    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
-                    { "id": 3, "price": 400, "name": "iPhone" },
-                    { "id": 4, "price": 900, "name": "iPad" },
-                    { "id": 5, "price": 600, "name": "Apple Watch "},
+  products: Product[] = [
+    {"id": 1, "price": 2800, "name": "MacBook Pro"},
+    {"id": 2, "price": 50, "name": "USB-C Adaptor"},
+    {"id": 3, "price": 400, "name": "iPhone"},
+    {"id": 4, "price": 900, "name": "iPad"},
+    {"id": 5, "price": 600, "name": "Apple Watch "},
 
 
-          ];
+  ];
 
 
-          form = new FormGroup({
-                    store: new FormGroup({
-                              branch: new FormControl(''),
-                              code: new FormControl('')
-                    }),
-                    selector: this.createStock({}),
-                    stock: new FormArray([
-                      this.createStock({ product_id: 1, quantity: 10 }),
-                      this.createStock({ product_id: 3, quantity: 50 }),
-                    ])
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl(''),
+      code: new FormControl('')
+    }),
+    selector: this.createStock({}),
+    stock: new FormArray([
+      this.createStock({product_id: 1, quantity: 10}),
+      this.createStock({product_id: 3, quantity: 50}),
+    ])
 
-          });
+  });
 
-          createStock(stock:any){
-            return new FormGroup({
-              product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
-              quantity: new FormControl(stock.quantity || 10),
-            });
-          }
+  createStock(stock: any) {
+    return new FormGroup({
+      product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
+      quantity: new FormControl(stock.quantity || 10),
+    });
+  }
 
-          onSubmit() {
-                    console.log('Submit:', this.form.value);
-          }
+  onSubmit() {
+    console.log('Submit:', this.form.value);
+  }
 
-          addStock(stock: any) {
-              const control = this.form.get('stock') as FormArray;
-              control.push(this.createStock(stock));
-          }
+  addStock(stock: any) {
+    const control = this.form.get('stock') as FormArray;
+    control.push(this.createStock(stock));
+  }
+
+  removeStock( {group, index}: { group:FormGroup, index:number}) {
+    const control = this.form.get('stock') as FormArray;
+    control.removeAt(index);
+  }
 }
