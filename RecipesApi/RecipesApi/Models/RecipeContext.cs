52bc3486c9088f2413eba92f5bc0789527c0700d
Jose Diaz <jose@exoreaction.com>
2024-10-25 19:27:44 -0500

    Task 4

--- RecipesApi/RecipesApi/Models/RecipeContext.cs
+++ RecipesApi/RecipesApi/Models/RecipeContext.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace RecipesApi.Models;
 
-public class RecipeContext
+public class RecipeContext: DbContext
 {
-    
+    public RecipeContext(DbContextOptions<RecipeContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Recipe> Recipes { get; set; } = null!;   
 }
