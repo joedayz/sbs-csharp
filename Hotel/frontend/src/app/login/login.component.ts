4f59b8ff58511035a27c42d4093ce7275765ae3a
Jose Diaz <jose@exoreaction.com>
2024-10-28 20:42:20 -0500

    task 12

--- Hotel/frontend/src/app/login/login.component.ts
+++ Hotel/frontend/src/app/login/login.component.ts
@@ -1,5 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatCard} from '@angular/material/card';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {NgIf} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {UsersService} from '../services/users.service';
 
 
 @Component({
@@ -7,24 +15,54 @@ import {RouterLink} from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbar,
+    MatCard,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    NgIf,
+    MatButton
   ],
   standalone: true
 })
 export class LoginComponent implements OnInit {
 
-  // Task 10: Add variables here
+  logInForm!: FormGroup;
 
   constructor(
-    // Task 10: Add private insrtances here
-
+    private formBuilder: FormBuilder,
+    private usersService: UsersService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    // Task 10: Add form here
-
+    this.logInForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
-  // Task 10: Add onSubmit() here
+  onSubmit(){
+    if (this.logInForm.invalid){
+      return;
+    }
+    const username = this.logInForm.get('username')?.value;
+    const password = this.logInForm.get('password')?.value;
+
+    this.usersService.login(username, password).subscribe({
+      next: (response) =>{
+        //Login successful
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('userId', response.id);
+        window.alert('Login successful');
+        this.router.navigate(['/']);
+      },
+      error: (error) =>{
+        window.alert('Login failed');
+      }
+    });
+  }
+
 
 }
