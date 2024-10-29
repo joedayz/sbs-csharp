bd6aeee72e4e9c67a336038a2ef058a2b8e84626
Jose Diaz <jose@exoreaction.com>
2024-10-28 21:33:00 -0500

    task 15

--- Hotel/frontend/src/app/reserve/reserve.component.ts
+++ Hotel/frontend/src/app/reserve/reserve.component.ts
@@ -1,7 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
-import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatCard, MatCardContent, MatCardHeader, MatCardImage} from '@angular/material/card';
 import {MatButton} from '@angular/material/button';
+import {NgForOf, NgIf} from '@angular/common';
+import {MatToolbar} from '@angular/material/toolbar';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {Room} from '../models/room.model';
+import {UsersService} from '../services/users.service';
+import {ReservationsService} from '../services/reservations.service';
+import {Router} from '@angular/router';
+import {catchError, of, tap} from 'rxjs';
 
 
 @Component({
@@ -13,29 +23,129 @@ import {MatButton} from '@angular/material/button';
     MatCard,
     MatCardHeader,
     MatCardContent,
-    MatButton
+    MatButton,
+    NgIf,
+    MatToolbar,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    NgForOf,
+    MatCardImage
   ],
   standalone: true
 })
 export class ReserveComponent implements OnInit {
-  // Task 13: Delcare variables here
+  reserveForm!: FormGroup;
+  //Parameter to show the complete reserve page to only logged-in users
+  showPage = true;
+  // Parameter to show the invoice if book now is clicked
+  showInvoice = false;
+
+  ratePerNight : number = 0;
+  taxes = 0;
+  totalRoomCost = 0;
+  capacity!:number;
+  totalNights :any;
+  totalBill =0;
+  type!:string
+  extraCharges = 0;
+  roomNo = 0;
+  token:any;
+  userid:any;
+  checkin:any;
+  checkout:any;
+  availableRooms:Array<Room>= [];
 
   constructor(
-    // Task 13: declare private instances here
-
+    private usersService: UsersService,
+    private reservationsService: ReservationsService,
+    private formBuilder: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    // Task 13: Declare form and store values
+    this.token = localStorage.getItem('token');
+    if(!this.token){
+      this.showPage = false;
+      window.alert("Please login to reserve a room");
+      this.router.navigate(['/login']);
+    }
+    this.userid = Number(localStorage.getItem('userId'));
+    console.log(localStorage.getItem('token'), this.userid);
+    this.reserveForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+    });
 
   }
 
-  // Task 13: Add getAvailableRooms() function here
+  getAvailableRooms(){
+    this.availableRooms = [];
+    const checkInDate = this.reserveForm.get('checkInDate')?.value;
+    const checkOutDate = this.reserveForm.get('checkOutDate')?.value;
 
-  // Task 13: Add roomAvailable() function here
+    this.reservationsService.getAvailableRooms(checkInDate, checkOutDate)
+      .pipe(
+        tap((response: any[]) => {
+          this.availableRooms = response.map(res => ({
+            Id: res.id,
+            Type: res.type,
+            Capacity: res.capacity,
+            RatePerDay: res.ratePerDay
+          }));
+          console.log(this.availableRooms);
+        }),
+        catchError((error) => {
+          window.alert("An unexpected error occurred while fetching available rooms!");
+          return of([]); // Devuelve un arreglo vacío para evitar romper el flujo
+        })
+      )
+      .subscribe();
+  }
 
-  // Task 13: Add close() function here
+  roomAvailable(id: number, capacity: number, ratePerDay: number, type:string){
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // You can also use 'auto' for instant scrolling
+    });
 
-  // Task 13: Add confirmReserve() method here
+    this.checkin = this.reserveForm.get('checkInDate')?.value;
+    this.checkout = this.reserveForm.get('checkOutDate')?.value;
+    const diffInMilliseconds = new Date(this.checkout).valueOf() - new Date(this.checkin).valueOf();
+    this.totalNights = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    this.showInvoice = true;
+    this.capacity = capacity;
+    this.ratePerNight = ratePerDay;
+    this.type = type;
+    this.roomNo = id;
+    this.totalRoomCost = this.totalNights * this.ratePerNight;
+    this.taxes = this.totalRoomCost * 0.18;
+    this.extraCharges = this.totalRoomCost * 0.10;
+    this.totalBill = this.totalRoomCost + this.taxes + this.extraCharges;
+  }
+
+  close(){
+    this.showInvoice = false;
+  }
+
+  confirmReserve(){
+    const checkInDate = this.reserveForm.get('checkInDate')?.value;
+    const checkOutDate = this.reserveForm.get('checkOutDate')?.value;
+    this.reservationsService.reserve(this.userid, this.roomNo, checkInDate, checkOutDate, this.totalBill)
+      .pipe(
+        tap((response) => {
+          console.log(response);
+          this.availableRooms = [];
+          window.alert("Room reserved successfully!");
+          this.router.navigate(['/dashboard']);
+          this.showInvoice = false;
+        }),
+        catchError((error) => {
+          window.alert("An unexpected error occurred while reserving the room!");
+          return of(null); // Devuelve un valor vacío para evitar romper el flujo
+        })
+      )
+      .subscribe();
+  }
 
 }
